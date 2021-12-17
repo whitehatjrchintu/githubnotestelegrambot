5645a0bf22607a41107d26f1a9b651619364cd38
whitehatjrchintu <74552895+whitehatjrchintu@users.noreply.github.com>
2021-12-17 20:54:52 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,77 @@
+# Github notes telegram bot
+
+With this telegram bot you can **create notes on [GitHub](https://www.github.com). You can even read [GitHub](https://www.github.com) uploaded files.** This TG bot is **heroku** based. Now before you continue i recommend you to read [**Prerequisites**](https://github.com/whitehatjrchintu/githubnotestelegrambot#Prerequisites-) section and [**What this bot can do?**](https://github.com/whitehatjrchintu/githubnotestelegrambot#What-this-bot-can-do) section.
+
+## Example:-
+
+screenshot 1 | screenshot 2
+--- | ---
+![photo_2021-10-20_15-46-43](https://user-images.githubusercontent.com/74552895/146566657-bd745d26-fc96-46a5-807d-ab955dedd89c.PNG) | ![photo_2021-10-20_15-46-51](https://user-images.githubusercontent.com/74552895/146566675-f9fa4f59-4ecd-4be3-8aac-bc9d00c2cfc4.PNG)
+
+
+## Prerequisites:-
+<details>
+<summary>
+  :information_source: Important information.
+</summary>
+
+  1. Create account on [GitHub](https://www.github.com) (if you haven't for only this script. i will recomment not to use personal account.).
+  2. Create account on [Heroku](https://dashboard.heroku.com) (if you haven't).
+  3. Create account on [Telegram](https://web.telegram.org) (if you haven't).
+  4. Go to [my.telegram.org/auth](https://my.telegram.org/auth), login and create app. Check [how to create app on telegram](https://core.telegram.org/api/obtaining_api_id). Now save api_id and api_hash which you got from [my.telegram.org/auth](https://my.telegram.org/auth).
+  5. Create a telegram bot by using [Bot Father](https://t.me/botfather). Check [how to create bot in telegram](https://core.telegram.org/bots#3-how-do-i-create-a-bot). [Bot Father](https://t.me/botfather) will give you bot token save that token.
+  6. Create repository on GitHub and create/upload an empty file in the same repository.
+  7. Create GitHub access token. Check [how to create GitHub access token](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token). Now save GitHub access token.
+  8. So now you have saved **six** things:-
+		- api_id
+		- api_hash
+		- bot_token
+		- github_access_token
+		- github_repository_link
+		- file_name_from_same_repository
+</details>
+
+## How to use?
+<details>
+  <summary>
+    Steps to use.
+  </summary>
+	
+#### Step 1:
+- Just git clone this repository.
+
+   `git clone https://github.com/whitehatjrchintu/githubnotestelegrambot.git`
+   
+   `cd githubnotestelegrambot`
+
+- Or download this [repository](https://github.com/whitehatjrchintu/githubnotestelegrambot/archive/main.zip) as zip.
+#### Step 2:
+- Now create repository (i will recommend to create private repository.) in your github account and upload all files.
+#### Step 3:
+- Copy your github repository's link and paste after **?template=** in this link `https://www.heroku.com/deploy/?template=`. Like this:-
+
+   `https://www.heroku.com/deploy/?template=https://github.com/whitehatjrchintu/githubnotestelegrambot`
+#### Step 5:
+- Now enter App name in **app_name** and **api_id**, **api_hash**, **bot_token**, **github_access_token**, **github_repository_link** and your **file_name_from_same_repository** which you saved in above steps, in **respective** asked field. Then click **Deploy app**.
+#### Step 6:
+- Finally go to your bot, click start button and save your notes.
+</details>
+
+## What this bot can do?
+<details>
+	<summary>
+		List of commands
+	</summary>
+	
+
+|commands|mean|
+|---|---|
+|#ls|list files that are in your github current repository.|
+|#read notes.txt|read a particular file which is available in your current repository.|
+|simple message|send a simple text/message and it will copy that text/message content into notes.txt|
+|a txt file|send a txt file and it will copy that txt file's content into notes.txt|
+	
+</details>
+
+### If you found any mistake or have any suggestion let me know i will correct/apply that.	
+## Meant for educational purpose only. I am not responsible if github or telegram block your account.
---
+++ bot.py
@@ -0,0 +1,172 @@
+from pyrogram.types import Message
+from pyrogram import Client, filters
+from github import Github
+from os import environ
+import os
+
+api_id = int(environ["API_ID"])
+api_hash = environ["API_HASH"]
+bot_token = environ["BOT_TOKEN"]
+github_access_token = environ["GITHUB_ACCESS_TOKEN"]
+github_repository_link = environ["GITHUB_REPOSITORY_LINK"]
+file_name_from_same_repository = environ["FILE_NAME_FROM_SAME_REPOSITORY"]
+app = Client(":memory:", api_id, api_hash, bot_token=bot_token)
+
+@app.on_message(filters.command(["start"]))
+def start(client: Client, message: Message):
+        msg = app.send_message(message.from_user.id,"Welcome",reply_to_message_id=message.message_id)
+
+@app.on_message(filters.regex(r'#read'))
+def get_all_files(client: Client, message: Message):
+        working_msg = app.send_message(message.from_user.id,"Working on it.",reply_to_message_id=message.message_id)
+        
+        #login
+        g = Github(github_access_token)
+
+        #getting text
+        msg_textedd = (message.text)
+        msg_texted = msg_textedd.lstrip("#read ")
+        print(msg_texted)
+        
+        #getting repo and files
+        repo = g.get_repo(github_repository_link)
+        contents = repo.get_contents(msg_texted, ref="main")
+        reading_file = contents.decoded_content.decode()
+        
+        #sending message
+        msg = app.send_message(chat_id=message.from_user.id, reply_to_message_id=message.message_id, text="The content of " + "<b>" + msg_texted + "</b>" + " file:" + "\n\n" + reading_file,parse_mode="html")
+
+        #delete message
+        working_msg.delete()
+
+@app.on_message(filters.regex(r'#ls'))
+def get_all_files(client: Client, message: Message):
+        working_msg = app.send_message(message.from_user.id,"Working on it.",reply_to_message_id=message.message_id)
+        
+        #login
+        g = Github(github_access_token)
+        
+        #getting repo and files
+        repo = g.get_repo(github_repository_link)
+
+        all_files = []
+        contents = repo.get_contents("")
+        while contents:
+                file_content = contents.pop(0)
+                if file_content.type == "dir":
+                    contents.extend(repo.get_contents(file_content.path))
+                else:
+                    file = file_content
+                    all_files.append(str(file).replace('ContentFile(path="','').replace('")',''))
+
+        all_filess = str(all_files).lstrip("['").rstrip("']")
+        
+        #sending message
+        msg = app.send_message(chat_id=message.from_user.id, reply_to_message_id=message.message_id, text="These files are in your current repository:" + "\n\n"+ "<b>" + all_filess + "</b>",parse_mode="html")
+
+        #delete message
+        working_msg.delete()
+
+@app.on_message(filters.text)
+def notes(client: Client, message: Message):
+        working_msg = app.send_message(message.from_user.id,"Working on it.",reply_to_message_id=message.message_id)
+        
+        #login
+        g = Github(github_access_token)
+        
+        #getting text
+        msg_texted = (message.text)
+        
+        #getting repo and file
+        repo = g.get_repo(github_repository_link)
+        contents = repo.get_contents(file_name_from_same_repository, ref="main")
+        aaa = contents.decoded_content.decode()
+
+        #copying old with new
+        aa = msg_texted + "\n" + aaa
+        f = open("file.txt","w")
+        f.write(aa)
+        f.close()
+
+        #removing duplicate
+        lines_seen = set()
+        outfile = open("filee.txt", "w")
+        for line in open("file.txt", "r"):
+            if line not in lines_seen:
+                outfile.write(line)
+                lines_seen.add(line)
+        outfile.close()
+
+        ff = open("filee.txt","r")
+        content = ff.read()
+
+        #updating file
+        repo.update_file(contents.path, "updated", content, contents.sha, branch="main")
+
+        #removing files
+        os.remove("file.txt")
+        os.remove("filee.txt")
+
+        #sending message
+        msg = app.send_message(message.from_user.id,"Done.",reply_to_message_id=message.message_id)
+
+        #delete message
+        working_msg.delete()
+
+@app.on_message(filters.document)
+def notes_file(client: Client, message: Message):
+        working_msg = app.send_message(message.from_user.id,"Working on it.",reply_to_message_id=message.message_id)
+        
+        #login
+        g = Github(github_access_token)
+        
+        #getting file id
+        file_id = None
+        if message.media:
+                if message.document:
+                    file_id = message.document.file_id
+
+        #downloading
+        download_file = app.download_media(file_id)
+        
+        #read file
+        with open(download_file, 'r') as file:
+                contentss = file.read()
+        
+        #getting repo and file
+        repo = g.get_repo(github_repository_link)
+        contents = repo.get_contents(file_name_from_same_repository, ref="main")
+        aaa = contents.decoded_content.decode()
+
+        #copying old with new
+        aa = contentss + "\n" + aaa
+        f = open("file.txt","w")
+        f.write(aa)
+        f.close()
+
+        #removing duplicate
+        lines_seen = set()
+        outfile = open("filee.txt", "w")
+        for line in open("file.txt", "r"):
+            if line not in lines_seen:
+                outfile.write(line)
+                lines_seen.add(line)
+        outfile.close()
+
+        ff = open("filee.txt","r")
+        content = ff.read()
+
+        #updating file
+        repo.update_file(contents.path, "updated", content, contents.sha, branch="main")
+
+        #removing files
+        os.remove("file.txt")
+        os.remove("filee.txt")
+
+        #sending message
+        msg = app.send_message(message.from_user.id,"Done.",reply_to_message_id=message.message_id)
+
+        #delete message
+        working_msg.delete()
+
+app.run()
